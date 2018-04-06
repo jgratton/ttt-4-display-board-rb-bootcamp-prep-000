574217c729a5d4732309aba3f90a9b50e960513b
John Gratton <jgratton@gmail.com>
2018-04-06 01:52:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@ board = Array.new(9, " ")
 
 def display_board board
   dashes = "-----------"
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts dashes
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts dashes
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
